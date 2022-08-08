76667f20ff6af790bbd462361dce04d5707b309d
Julian Chae <Julianchae9@gmail.com>
2022-08-08 12:38:12 -0600

    Basic User mapping

--- ECommerce/src/main/java/com/skilldistillery/entities/User.java
+++ ECommerce/src/main/java/com/skilldistillery/entities/User.java
@@ -26,6 +26,54 @@ public class User {
 	private String firstName;
 	@Column(name= "last_name")
 	private String lastName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 	
 
