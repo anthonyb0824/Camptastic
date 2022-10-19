e3309940f2aee7a7ab434622c31c936fdbc9ea7d
Julian Chae <Julianchae9@gmail.com>
2022-10-19 12:13:55 -0600

    Small

--- ECommerceBoot/src/main/java/com/skilldistillery/repositories/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skilldistillery.entities.Item;
-
-public interface ItemRepo extends JpaRepository<Item, Integer> {
-
-}
